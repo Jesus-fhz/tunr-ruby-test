979b8bc3f9bcc10541f60da354a26725cfaca60b
Jesus-fhz <jesusflores9325@gmail.com>
2022-02-01 11:36:18 +1100

    Fixing bug validation

--- app/models/mixtape.rb
+++ app/models/mixtape.rb
@@ -1,5 +1,5 @@
 class Mixtape < ApplicationRecord
     has_and_belongs_to_many :songs
     belongs_to :user, optional: true
-    validates :name, length:{ minimum: 2, maximum: 20}
+    validates :name
 end
